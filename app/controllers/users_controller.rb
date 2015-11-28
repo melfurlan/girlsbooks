7168290c9221605dcdbf27a182fd63a7b9c898b7
Karen Miki Kuba <karen@hotmail.com>
2015-11-28 16:59:18 -0200

    show e destroy de usuarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,15 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def destroy
+		user = User.find(params[:id])
+		user.destroy
+		redirect_to action: :index
+	end
 
 	private
 
