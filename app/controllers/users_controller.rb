290ea0a785a493730ef622e0d183c316b4522114
Karen Miki Kuba <karen@hotmail.com>
2015-11-28 16:26:48 -0200

    edicao de cadastro

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,21 @@ class UsersController < ApplicationController
 		redirect_to action: :index
 	end
 
+	def update
+		user = User.find(params[:id])
+		user.update_attributes(accessible_params)
+		redirect_to action: :index
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+
 	private
 
 	def accessible_params
     	params.require(:user).permit(:name, :email, :birth)
   	end
+
 end
